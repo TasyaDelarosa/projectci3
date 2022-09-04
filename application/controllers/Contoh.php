965a118703a6c6cd00aeb45df06fe4e2634a9282
tasya <tasyadelarosa@gmail.com>
2022-09-04 23:48:16 +0800

    firstinit

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contoh extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('contoh_view');
+	}
+}
+
+?>
---
+++ application/views/contoh_view.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Porto Tasya</title>
+    <link href="<?php echo base_url();?>resources/css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<!-- header -->
+	<header>
+		<div class="container">
+			<h1><a>Tasya Delarosa</a></h1>
+			<ul>
+                <li><a href="https://www.facebook.com/tasya.delarosa/"><i class="fab fa-facebook"></i></a></li>
+				<li><a href="https://instagram.com/tasyadelarosa/"><i class="fab fa-instagram"></i></a></li>
+				<li><a href="www.linkedin.com/in/tasyadelarosa/"><i class="fab fa-linkedin"></i></a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- banner -->
+	<section class="banner">
+		<h2>Welcome To My First Webstie</h2>
+	</section>
+
+	<!-- about -->
+	<section class="about">
+		<div class="container">
+			<h3>ABOUT</h3>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+	</section>
+
+	<!-- service -->
+	<section class="service">
+		<div class="container">
+			<h3>EXPERIENCE</h3>
+			<div class="box">
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-school"></i></div>
+					<h4>Hasanuddin University - Informatics</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-globe"></i></div>
+					<h4>GDSC - Core Team</h4>
+				</div>
+                <div class="col-4">
+					<div class="icon"><i class="fas fa-paint-brush"></i></div>
+					<h4>UI Design</h4>
+				</div>
+				<div class="col-4">
+					<div class="icon"><i class="fas fa-comment"></i></div>
+					<h4>Communication</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<footer>
+		<div class="container">
+			<small>2022 - Tasya Delarosa</small>
+		</div>
+	</footer>
+
+</body>
+</html>
